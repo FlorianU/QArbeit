f4e790fc3bff97bed93795128ad0a9b12f4f7e8a
Nino Cabernard <35307422+NinoCabernard@users.noreply.github.com>
2018-06-08 11:17:39 +0200

    Register angepasst

--- Projekt/Spielverleih/Spielverleih/Account/Register.aspx.cs
+++ Projekt/Spielverleih/Spielverleih/Account/Register.aspx.cs
@@ -17,17 +17,11 @@ namespace Spielverleih.Account
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
             var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
-
             
             IdentityResult result = manager.Create(user, Password.Text);
 
             if (result.Succeeded)
             {
-                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                //string code = manager.GenerateEmailConfirmationToken(user.Id);
-                //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
-                //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
-
                 signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
                 var benutzer = new Benutzer()
                 {
@@ -39,13 +33,11 @@ namespace Spielverleih.Account
                     PLZ = int.Parse(Plz.Text),
                     Ort = Ort.Text
                 };
-                manager.AddToRole<ApplicationUser, string>(user.Id, "Kunde");
+                manager.AddToRole(user.Id, "Kunde");
                 var db = new LudothekDBEntities();
                 db.Benutzer.Add(benutzer);
                 db.SaveChanges();
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-
-
             }
             else 
             {
